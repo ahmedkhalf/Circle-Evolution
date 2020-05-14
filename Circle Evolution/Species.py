97746aefd6f45a51d63801f38faa77f993beaf13
Ahmed Khalf <36672196+ahmedkhalf@users.noreply.github.com>
2020-05-14 18:33:39 +0400

    Added Evolution Class

--- Circle Evolution/Species.py
+++ Circle Evolution/Species.py
@@ -2,6 +2,8 @@ from skimage.metrics import structural_similarity as ss
 from skimage.draw import circle
 import matplotlib.pyplot as plt
 import numpy as np
+import random
+import copy
 import cv2
 
 
@@ -36,10 +38,45 @@ class Specie:
                            row[2] * radiusAvg, row[3])
 
     def getFitness(self, target):
+        return (np.square(self.phenotype - target)).mean(axis=None)
         return ss(self.phenotype, target)
 
 
-s = Specie((64, 64))
-s.render()
+class Evolution:
+    def __init__(self, target, settings):
+        self.target = target
+        self.settings = settings
+        self.species = [Specie((64, 64)) for x in range(1)]
 
-Helper.showImage(s.img)
+    def mutate(self, specie):
+        ns = copy.deepcopy(specie)
+        ra = np.random.rand(150, 4) < 0.1
+        if random.random() > 0.5:
+            ns.genotype[ra] = np.random.rand(len(ns.genotype[ra]))
+        else:
+            ns.genotype[ra] += np.random.rand(len(ns.genotype[ra])) / 10
+        return ns
+
+    def evolve(self):
+        # Go to next generation
+        for specie in self.species:
+            specie.render()
+            fit = specie.getFitness(self.target)
+            print('{0:.16f}'.format(fit))
+            if fit < 10:
+                Helper.showImage(specie.phenotype)
+            specie.phenotype = np.zeros((64, 64))
+
+            ns = self.mutate(specie)
+            ns.render()
+            nfit = ns.getFitness(self.target)
+
+            if abs(nfit) < abs(fit):
+                self.species[0] = ns
+                self.species[0].phenotype = np.zeros((64, 64))
+
+
+target = Helper.loadTargetImage("Images/Mona Lisa 64.jpg", (64, 64))
+e = Evolution(target, None)
+while True:
+    e.evolve()
