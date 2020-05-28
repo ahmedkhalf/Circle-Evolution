98fc410f595c8350785b42b01f79f52d35cc4aca
ahmedkhalf <ahmedkhalf567@gmail.com>
2020-05-28 22:27:20 +0400

    Made load_target_image color easier to set

--- circle_evolution/helpers.py
+++ circle_evolution/helpers.py
@@ -6,7 +6,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-def load_target_image(image_path, color=cv2.IMREAD_COLOR, size=None):
+def load_target_image(image_path, color=True, size=None):
     """Loads images from image path.
 
     Loads and converts image to given colorspace for later processing using
@@ -14,7 +14,8 @@ def load_target_image(image_path, color=cv2.IMREAD_COLOR, size=None):
 
     Args:
         image_path (str): path to load the image.
-        color: optional variable for colorspace conversion.
+        color (bool): if true the image is loaded as rgb, if false grayscale.
+            Defaults to true.
 
     Returns:
         Image loaded from the path as a numpy.ndarray.
@@ -24,7 +25,14 @@ def load_target_image(image_path, color=cv2.IMREAD_COLOR, size=None):
     """
     if not os.path.exists(image_path):
         raise FileNotFoundError(f"Image was not found at {image_path}")
-    target = cv2.imread(image_path, color)
+
+    if color:
+        target = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        # Switch from bgr to rgb
+        target = target[:, :, (2, 1, 0)]
+    else:
+        target = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
     if size:
         # Only resizes image if it is needed!
         target = cv2.resize(src=target, dsize=size, interpolation=cv2.INTER_AREA)
